8352f8fd508823ba3d5b1d7947a000f304bf2c9c
Logon Aluno <logonaluno@local.com>
2020-02-06 22:57:28 -0300

    aula 03

---
+++ cervejaria/src/main/java/br/com/fiap/cervejaria/service/UserService.java
@@ -0,0 +1,8 @@
+package br.com.fiap.cervejaria.service;
+
+import br.com.fiap.cervejaria.dto.CreateUserDTO;
+import br.com.fiap.cervejaria.dto.UserDTO;
+
+public interface UserService {
+    UserDTO create(CreateUserDTO createUserDTO);
+}
