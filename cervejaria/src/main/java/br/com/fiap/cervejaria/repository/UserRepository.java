786748cf2d91ce1525cbf632c09ecafcc9536478
Logon Aluno <logonaluno@local.com>
2020-02-15 10:03:16 -0300

    aula 04

--- cervejaria/src/main/java/br/com/fiap/cervejaria/repository/UserRepository.java
+++ cervejaria/src/main/java/br/com/fiap/cervejaria/repository/UserRepository.java
@@ -4,4 +4,5 @@ import br.com.fiap.cervejaria.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findFirstByUsername(String username);
 }
